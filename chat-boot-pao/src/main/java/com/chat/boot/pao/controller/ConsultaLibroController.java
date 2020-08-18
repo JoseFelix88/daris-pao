03f77ccebd0aa977698ace6c6cf482696256c4f6
JOSE CURIEL <josecuriel88@gmail.com>
2020-08-17 22:04:31 -0500

    cambio en la configuracion de la base de datos
    cambio en la configuracion de la base de datos
    
    Co-Authored-By: josefelix88 <16869165+josefelix88@users.noreply.github.com>

--- chat-boot-pao/src/main/java/com/chat/boot/pao/controller/ConsultaLibroController.java
+++ chat-boot-pao/src/main/java/com/chat/boot/pao/controller/ConsultaLibroController.java
@@ -6,7 +6,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.chat.boot.pao.service.LibroService;
@@ -24,8 +26,15 @@ public class ConsultaLibroController implements Serializable {
 	@Autowired 
 	private LibroService libroService;
 	
-	@PostMapping("/consulta/libro")
-	public String consultarLibro(@ModelAttribute("search") String search) {
+	@GetMapping("/index")
+	public String index(Model model) {
+		model.addAttribute("search", "");
+		return "index";
+	}
+	
+	@PostMapping("/consulta/libro/{search}")
+	public String consultarLibro(@PathVariable("search") String search) {
+		log.info("Search: "+ search);
 		libroService.listarLibros().forEach(libro -> log.info(libro.toString()));
 		return "index";
 	}
