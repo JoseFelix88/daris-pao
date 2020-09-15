5e7e1a163f0c511f7910ef5323c8771569676129
JOSE CURIEL <josecuriel88@gmail.com>
2020-09-15 00:02:46 -0500

    optimizacion de la busqueda
    optimizacion de la busqueda

--- chat-bot-pao/src/main/java/com/chat/bot/pao/model/PalabrasClave.java
+++ chat-bot-pao/src/main/java/com/chat/bot/pao/model/PalabrasClave.java
@@ -12,7 +12,7 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "PALABRASCLAVE")
-public class palabrasclave implements Serializable {
+public class PalabrasClave implements Serializable {
 
 	/**
 	 * 
